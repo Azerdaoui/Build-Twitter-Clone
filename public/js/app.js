7df7949b1be4a847a5136bad7f03e17ccafb647f
Azerdaoui Othmane <othmane.azerdaoui@gmail.com>
2020-07-05 01:05:54 +0100

    styling the base template

--- public/js/app.js
+++ public/js/app.js
@@ -49865,7 +49865,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/src/index.js):\nSyntaxError\n\n(9:3) `@apply` cannot be used with `.text-lueb` because `.text-lueb` either cannot be found, or its actual definition includes a pseudo-selector like :hover, :active, etc. If you're sure that `.text-lueb` exists, make sure that any `@import` statements are being properly processed *before* Tailwind CSS sees your CSS, as `@apply` can only be used for classes in the same CSS tree.\n\n \u001b[90m  7 | \u001b[39m\n \u001b[90m  8 | \u001b[39ma \u001b[33m{\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m  9 | \u001b[39m  \u001b[36m@apply\u001b[39m text-lueb\u001b[33m;\u001b[39m\n \u001b[90m    | \u001b[39m  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 10 | \u001b[39m\u001b[33m}\u001b[39m\n\n    at /Users/othmane/Projects/Work/twitter/node_modules/webpack/lib/NormalModule.js:316:20\n    at /Users/othmane/Projects/Work/twitter/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/othmane/Projects/Work/twitter/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/othmane/Projects/Work/twitter/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at /Users/othmane/Projects/Work/twitter/node_modules/postcss-loader/src/index.js:207:9");
 
 /***/ }),
 
